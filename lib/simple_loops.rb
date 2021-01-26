3cf659b7993075c51fdf2e695fc8c16338909b6a
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-26 23:04:00 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,3 +1,11 @@
 # Write your methods here
 
-def loop_message_five_times()
+def loop_message_five_times(string)
+  count = 0
+  while count < 5 do
+    puts string
+    count += 1
+  end
+end
+
+def loop_mes
