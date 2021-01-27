89ef6fbd25c9bfddfe94e42e03e04adfbc92df34
Nicholas Degrate <nicholasdegrate@gmail.com>
2021-01-27 00:09:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,13 +1,12 @@
 # Write your methods here
 
-def loop_message_five_times(string)
+def loop_message_five_times(message)
   count = 0
   while count < 5 do
-    puts string
+    puts message
     count += 1
   end
 end
-
 def loop_message_n_times(string, num)
   count = 0
   while count <= num do
